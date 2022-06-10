543fb54630306be23a13d71ea272a65277bfcec0
judeallanhill <judeallanhill@icloud.com>
2022-06-10 13:44:15 -0700

    Adding Animation (BUGGED)

--- src/Game.ts
+++ src/Game.ts
@@ -136,7 +136,6 @@ class GridView extends BaseParentView {
     }
 
 }
-
 let numColumns = 5;
 let numRows = 2;
 
@@ -570,7 +569,66 @@ export async function start() {
         turn_to(new Point(+0, baken.vx))
                 if (baken.FruitVisable === false) {
         }
-    }
+        const scale = 2;
+
+        let img = new Image();
+        img.src = './assets/Free/Virtual Guy/Run (32x32).png';
+        img.onload = function() {
+        init();
+        };
+
+        let canvas = document.querySelector('canvas');
+        let ctx = canvas.getContext('2d');
+        const width = 32;
+        const height = 30;
+        const scaledWidth = scale * width;
+        const scaledHeight = scale * height;
+
+        function drawFrame(frameX, frameY, canvasX, canvasY) {
+        ctx.drawImage(img,
+                        frameX * width, frameY * height, width, height,
+                        canvasX, canvasY, scaledWidth, scaledHeight);
+        }
+
+        function init() {
+        // drawFrame(0, 0, 0, 0);
+        // drawFrame(1, 0, scaledWidth, 0);
+        // drawFrame(0, 0, scaledWidth * 2, 0);
+        //  drawFrame(2, 0, scaledWidth * 3, 0);
+        }
+
+
+        window.requestAnimationFrame(step);
+
+        function step() {
+        // do something
+        window.requestAnimationFrame(step);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawFrame(0, 0, 0, 0);
+        // repeat for each frame
+        }
+
+
+const cycleLoop = [0, 1, 0, 2];
+let currentLoopIndex = 0;
+let frameCount = 0;
+
+function step2() {
+  frameCount++;
+  if (frameCount < 15) {
+    window.requestAnimationFrame(step);
+    return;
+  }
+  frameCount = 3;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
+  currentLoopIndex++;
+  if (currentLoopIndex >= cycleLoop.length) {
+    currentLoopIndex = 0;
+  }
+  window.requestAnimationFrame(step);
+}
+}
 
     restart()
 
